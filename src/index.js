eae73dd76022199eede92a86727cb9243f1f6d60
ssheaks <sarah.sheaks@gmail.com>
2018-04-18 09:54:25 -0700

    clean it up

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import './index.css';
 import store from './store';
-import App from './App';
+import ComplaintForm from './complaint-form';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
 <Provider store={store}>
-  <App />
+  <ComplaintForm />
 </Provider>,
  document.getElementById('root'));
 registerServiceWorker();
