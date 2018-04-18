0472bb99e78d4e9c0f797555170de394d7a026be
ssheaks <sarah.sheaks@gmail.com>
2018-04-18 13:11:38 -0700

    minor CSS tweaks and api integration

--- src/complaint-form.js
+++ src/complaint-form.js
@@ -1,22 +1,90 @@
 import React, { Component } from 'react';
-import {reduxForm, Field} from 'redux-form';
+import {reduxForm, Field, SubmissionError, focus} from 'redux-form';
 import Input from './components/input';
 import {required, nonEmpty, lengthFive, numbers} from './validators';
 import './App.css';
 
 class ComplaintForm extends Component {
+  onSubmit(values) {
+    return fetch(' https://us-central1-delivery-form-api.cloudfunctions.net/api/report', {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        'Content-Type': 'application/json'
+      // "trackingNumber": "01234", // This should be a string - leading zeros are allowed
+      // "issue": "not-delivered", // One of "not-delivered", "wrong-item", "missing-part", "damaged", or "other"
+      // "details": "It wasn't delivered" // Optional
+        }
+      })
+        .then(res => {
+          if(!res.ok) {
+            if (
+              res.headers.has('content-type') &&
+              res.headers
+                .get('content-type')
+                .startsWith('application/json')
+            ) {
+              return res.json().then(err => Promise.reject(err));
+            }
+
+            return Promise.reject({
+              code: res.status,
+              message: res.statusText 
+            });
+          }
+          return;
+        })
+        .then(() => console.log('Submitted with values', values))
+        .catch(err => {
+          const{reason, message, location} = err;
+          if (reason === 'ValidationError') {
+            return Promise.reject(
+              new SubmissionError({
+                [location]: message
+              })
+            );
+          }
+          return Promise.reject(
+            new SubmissionError({
+              _error: 'Error submitting message'
+            })
+          );
+        });
+  }
   render() {
+    let successMessage;
     if (this.props.submitSucceeded) {
-      return <div className="submitSuccess">Submit successful</div>;
+      successMessage = (
+        <div className="message message-success">
+        Message submitted successfully
+    </div>
+      );
     }
+
+    let errorMessage;
+        if (this.props.error) {
+            errorMessage = (
+                <div className="message message-error">{this.props.error}</div>
+            );
+        }
+
+    // if (this.props.submitSucceeded) {
+    //   return <div className="submitSuccess">Submit successful</div>;
+    // }
+
+    // if (this.props.error) {
+    //   return <div className="submitSuccess">Submit successful</div>;
+    // }
+
     return (
       <div className="complaintForm">
         <header className="App-header">
           <h1 className="App-title">Delivery Complaint</h1>
         </header>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values)
-        // this.onSubmit(values)
+        <form onSubmit={this.props.handleSubmit(values => this.onSubmit(values)
           )}>
+          {successMessage}
+          {errorMessage}
           <div>
             <label value="Tracking number">Tracking number</label>
             <Field 
@@ -30,7 +98,8 @@ class ComplaintForm extends Component {
           <div>
             <label htmlFor="issue">What is your issue?</label>
             <Field 
-            component="select"
+            component={Input}
+            element="select"
             name="issue" 
             id="issue"
             >
